ba4e5388a9a617edb04149f1f0966443c4f2b127
Sylvvvvia2001 <139299802+Sylvvvvia2001@users.noreply.github.com>
2025-01-29 14:58:01 +0800

    Add files via upload

--- official_dev/OpenHAB_Official_Integration_Code/Eufy_Doorbell/org.openhab.binding.eufydoorbell/IpCameraConfiguration.java
+++ official_dev/OpenHAB_Official_Integration_Code/Eufy_Doorbell/org.openhab.binding.eufydoorbell/IpCameraConfiguration.java
@@ -1,1 +1,12 @@
 
+
+package org.openhab.binding.ipcamera.internal;
+
+
+public class IpCameraConfiguration {
+
+    /**
+     * Sample configuration parameter. Replace with you own.
+     */
+    public String config1;
+}
